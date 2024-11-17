5ee78e690db4ba1ab6b09ec293dad4deff3a231e
qcc42 <qcc.1992@gmail.com>
2024-11-17 17:55:24 +0200

    Solution for assignment 4.6.

--- tests/correct_amount_get.test.js
+++ tests/correct_amount_get.test.js
@@ -7,6 +7,8 @@ const app = require('../app')
 const Blog = require('../models/blog')
 const api = supertest(app)
 const maxLikes = require('../utils/max_likes')
+const mostBlogs = require('../utils/mostBlogs')
+
 
 it('correct amount of blogs are returned', async () => {
    await Blog.deleteMany({}) 
@@ -190,8 +192,77 @@ it('max_likes() work', () => {
       likes: 2,
       __v: 0
     }  
+
+
   ]
 
   assert.deepStrictEqual(maxLikes(blogs), blogs[2])
 
+})
+
+
+it('test mostBlogs()', () => {
+
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+,
+
+  {
+    _id: "5a422bc61b54a676235d17fc",
+    title: "Type wars2",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars2.html",
+    likes: 2,
+    __v: 0
+  } 
+  ]
+
+  console.log(mostBlogs(blogs))
 })
